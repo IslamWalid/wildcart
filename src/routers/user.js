1d51ad278bc7fac3c25a171e21b7f082faf710de
IslamWalid <islamwalid83@gmail.com>
2023-02-26 01:46:38 +0200

    feat: implement user login and logout

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const { register } = require('../controllers/user');
+const passport = require('../configs/passport-config')(require('passport'));
 
 const router = express.Router();
 
-router.post('/register/', register);
+router.post('/register', register);
+
+router.post('/login', (req, res) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+
+    if (!user) {
+      res.status(401).json(info);
+      return;
+    }
+
+    req.login(user, (err) => {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+
+      res.sendStatus(200);
+    });
+  })(req, res);
+});
+
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+
+    res.sendStatus(200);
+  });
+});
 
 module.exports = router;
--- test/unit/services/user.test.js
+++ test/unit/services/user.test.js
@@ -1,8 +1,31 @@
 require('dotenv').config();
 
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const req = require('supertest');
 const app = require('../../../app');
-const { sequelize } = require('../../../src/models/');
+const { sequelize, User, Customer } = require('../../../src/models/');
+
+async function createUser() {
+  const id = crypto.randomUUID();
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash('StrongPassword123!', salt);
+
+  await User.create({
+    id: crypto.randomUUID(),
+    username: 'john_doe',
+    firstName: 'John',
+    lastName: 'Doe',
+    password: hash,
+    address: 'some address',
+    phone: '+201012345678',
+    userType: 'customer',
+    customer: { id }
+  },
+  {
+    include: Customer
+  });
+}
 
 beforeEach(async () => {
   await sequelize.query('TRUNCATE TABLE "user" CASCADE');
@@ -36,9 +59,33 @@ describe('user endpoints', () => {
     const res = await req(app).post('/user/register').send(reqBody);
     expect(res.statusCode).toBe(400);
   });
-  it.todo('should login with valid credentials');
-  it.todo('should login with invalid credentials');
-  it.todo('should logout');
+
+  it('should login with valid credentials', async () => {
+    const reqBody = {
+      username: 'john_doe',
+      password: 'StrongPassword123!'
+    };
+    await createUser();
+    const res = await req(app).post('/user/login').send(reqBody);
+    expect(res.statusCode).toBe(200);
+    expect(res.headers['set-cookie']).toBeDefined();
+  });
+
+  it('should login with invalid credentials', async () => {
+    const reqBody = {
+      username: 'john_doe',
+      password: 'incorrect password'
+    };
+    await createUser();
+    const res = await req(app).post('/user/login').send(reqBody);
+    expect(res.statusCode).toBe(401);
+    expect(res.body.msg).toBe('incorrect password');
+  });
+
+  it('should logout', async () => {
+    const res = await req(app).get('/user/logout');
+    expect(res.statusCode).toBe(200);
+  });
 });
 
 afterAll(async () => {
