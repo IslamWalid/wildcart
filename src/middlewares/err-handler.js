45cb0b701d26138ed781fa189dc5069f9619f482
IslamWalid <islamwalid83@gmail.com>
2023-03-01 15:13:45 +0200

    fix(database): make `phone` attribute unique

--- src/middlewares/err-handler.js
+++ src/middlewares/err-handler.js
@@ -4,13 +4,18 @@ const { UniqueConstraintError } = require('sequelize');
 const errHandler = async (err, req, res, next) => {
   log.debug(err);
   if (err instanceof UniqueConstraintError) {
+    const { username, phone } = err.fields;
     log.info({
       message: 'element already exist',
       fields: err.fields,
       status: 409,
       table: err.original.table
     });
-    res.status(409).json({ message: 'username already exists' });
+    if (username) {
+      res.status(409).json({ message: 'username already exists' });
+    } else if (phone) {
+      res.status(409).json({ message: 'phone number is already used' });
+    }
   } else if (err.status) {
     log.info(err);
     res.status(err.status).json({ message: err.message });
---
+++ src/migrations/20230301130452-change-phone-column-constraints.js
@@ -0,0 +1,16 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('user', 'phone', {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('user', 'phone', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+  }
+};
