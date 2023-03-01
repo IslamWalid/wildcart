5a229540886363a71881d0cf5bbaa26ebb66f668
IslamWalid <islamwalid83@gmail.com>
2023-03-01 20:22:40 +0200

    refactor: validate input in separate module
    test(user): fix the seeding data

---
+++ test/unit/utils.test.js
@@ -0,0 +1,65 @@
+const { validateInput, inputTypes } = require('../../src/utils/validate-input');
+
+describe('validate register input', () => {
+  it('should pass valid input', () => {
+    const input = {
+      username: 'john_doe',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'StrongPassword123!',
+      address: 'some address',
+      phone: '+201012345678',
+      userType: 'customer'
+    };
+    expect(validateInput(input, inputTypes.REGISTER)).toBeNull();
+  });
+
+  it('should pass input with missing fields', () => {
+    const input = {
+      username: 'john_doe',
+      firstName: 'John',
+      lastName: 'Doe',
+      phone: '+201012345678'
+    };
+    expect(validateInput(input, inputTypes.REGISTER))
+      .toBe('required fields are missing');
+  });
+
+  it('should pass input with weak password', () => {
+    const input = {
+      username: 'john_doe',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: '123',
+      address: 'some address',
+      phone: '+201012345678',
+      userType: 'customer'
+    };
+    expect(validateInput(input, inputTypes.REGISTER))
+      .toBe('weak password');
+  });
+
+  it('should pass input with invalid phone number', () => {
+    const input = {
+      username: 'john_doe',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'StrongPassword123!',
+      address: 'some address',
+      phone: '123',
+      userType: 'customer'
+    };
+    expect(validateInput(input, inputTypes.REGISTER))
+      .toBe('invalid phone number');
+  });
+});
+
+describe('validate login input', () => {
+  it('should pass input with missing fields', () => {
+    const input = {
+      username: 'john_doe'
+    };
+    expect(validateInput(input, inputTypes.REGISTER))
+      .toBe('required fields are missing');
+  });
+});
