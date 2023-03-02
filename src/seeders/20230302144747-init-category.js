fa6bc94fab193c7b8ef34bfc136b9255da6db6f0
IslamWalid <islamwalid83@gmail.com>
2023-03-02 17:00:03 +0200

    chore(database): init category table with seeder

---
+++ src/seeders/20230302144747-init-category.js
@@ -0,0 +1,22 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const categories = [
+      { name: 'books' },
+      { name: 'sports' },
+      { name: 'clothes' },
+      { name: 'mobiles' },
+      { name: 'laptops' },
+      { name: 'computers' },
+      { name: 'supermarket' },
+      { name: 'men fashion' },
+      { name: 'kids fashion' },
+      { name: 'women fashion' }
+    ];
+
+    await queryInterface.bulkInsert('category', categories);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('category', null, {});
+  }
+};
