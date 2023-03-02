e3a714c711cc9ed6a49f88447fe5c41de52b0292
IslamWalid <islamwalid83@gmail.com>
2023-03-02 16:35:00 +0200

    fix: use transactions in creating with assocations
    creating with assocations uses to separate sql queries to create two
    differenct rows in differenct tables, using a transaction to perform
    this operation is a must to ensure creating the two rows together

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 const bcrypt = require('bcrypt');
-const { User, Customer, Seller } = require('../models');
+const { sequelize, User, Customer, Seller } = require('../models');
 
 async function createUser(userData) {
   const id = crypto.randomUUID();
@@ -11,13 +11,15 @@ async function createUser(userData) {
   userData.id = id;
   userData.password = hash;
 
-  if (userData.userType === 'customer') {
-    userData.Customer = { id };
-    await User.create(userData, { include: Customer });
-  } else {
-    userData.Seller = { id, shopName: userData.shopName };
-    await User.create(userData, { include: Seller });
-  }
+  await sequelize.transaction(async (t) => {
+    if (userData.userType === 'customer') {
+      userData.Customer = { id };
+      await User.create(userData, { include: Customer, transaction: t });
+    } else {
+      userData.Seller = { id, shopName: userData.shopName };
+      await User.create(userData, { include: Seller, transaction: t });
+    }
+  });
 }
 
 module.exports = {
