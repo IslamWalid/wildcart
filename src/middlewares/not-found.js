6ef2055d5d9c186b47cd4b172b11505798e7ece9
IslamWalid <islamwalid83@gmail.com>
2023-03-06 02:59:49 +0200

    feat: add `not-found` middlewares to handle unexpected routes

---
+++ src/middlewares/not-found.js
@@ -0,0 +1,16 @@
+const log = require('../configs/log-config');
+
+const notFound = async (req, res, next) => {
+  log.warn({
+    message: 'route not found',
+    meta: {
+      method: req.method,
+      path: req.path,
+      query: req.query,
+      body: req.body
+    }
+  });
+  res.status(404).json({ message: 'resource not found' });
+};
+
+module.exports = notFound;
