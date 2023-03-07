67160eafe44a7612a4e7dc82b094642761575c4c
IslamWalid <islamwalid83@gmail.com>
2023-03-07 14:40:03 +0200

    feat: add `/users/details` endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
-const { register, login, logout } = require('../controllers/user');
+const { register, login, logout, userDetails } = require('../controllers/user');
 
 const router = express.Router();
 
 router.post('/register', register);
 
 router.post('/login', login);
+
+router.get('/details/:id', userDetails);
 
 router.get('/logout', logout);
 
