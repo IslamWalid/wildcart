cbd00b6ac86296c93467b399aad1207264882553
IslamWalid <islamwalid83@gmail.com>
2023-03-14 02:07:54 +0200

    refactor: make `create-res-err` more general
    change the structure of the file to make it easier to create response
    errors for various types of errors rather than database errors

--- src/utils/create-res-err.js
+++ src/utils/create-res-err.js
@@ -8,11 +8,17 @@ const log = require('../configs/log-config');
 
 const DATA_TYPE_ERROR_CODE = '22P02';
 
-function getErrInfo(err) {
+function databaseErrInfo(err) {
+  log.debug({
+    message: 'parsing database error info',
+    meta: err
+  });
+
   switch (true) {
     case err instanceof UniqueConstraintError:
       return {
         message: err.parent.detail,
+        stack: err.stack,
         meta: {
           name: err.name,
           sql: err.parent.sql,
@@ -25,6 +31,7 @@ function getErrInfo(err) {
     case err instanceof ForeignKeyConstraintError:
       return {
         message: err.original.detail,
+        stack: err.stack,
         meta: {
           name: err.name,
           sql: err.original.sql,
@@ -37,6 +44,7 @@ function getErrInfo(err) {
     case err instanceof DatabaseError:
       return {
         message: err.message,
+        stack: err.stack,
         meta: {
           name: err.name,
           sql: err.sql,
@@ -46,35 +54,53 @@ function getErrInfo(err) {
       };
 
     default:
-      return {
-        message: 'unexpected error',
-        meta: err
-      };
+      return null;
   }
 }
 
-function createResErr(err) {
+function createDatabaseResErr(err) {
   log.debug({
-    message: 'debug error before calling getErrInfo in createResErr',
+    message: 'creating database response error',
     meta: err
   });
 
-  const errInfo = getErrInfo(err);
+  const errInfo = databaseErrInfo(err);
+  if (!errInfo) {
+    return null;
+  }
 
   switch (errInfo.meta.constraint) {
     case DATA_TYPE_ERROR_CODE:
       return { status: 400, message: 'invalid input datatype', errInfo };
     case 'product_category_category_name_fkey':
       return { status: 400, message: 'invalid categories', errInfo };
+    case 'image_product_id_fkey':
+      return { status: 404, message: 'product does not exist', errInfo };
+    case 'image_pkey':
+      return { status: 409, message: 'product already has an image', errInfo };
     case 'user_username_key':
       return { status: 409, message: 'username already exists', errInfo };
     case 'user_phone_key':
       return { status: 409, message: 'phone already exists', errInfo };
     case 'product_name_seller_id_unique_constraint':
       return { status: 409, message: 'product name already exists for this seller', errInfo };
+    default:
+      return null;
+  }
+}
+
+function createResErr(err) {
+  log.debug({
+    message: 'creating response error',
+    meta: err
+  });
+
+  const resErr = createDatabaseResErr(err);
+  if (resErr) {
+    return resErr;
   }
 
-  return { status: 500, message: 'unexpected error', errInfo };
+  return { status: 500, message: 'unexpected error', errInfo: err };
 }
 
 module.exports = createResErr;
