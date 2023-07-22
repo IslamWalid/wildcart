6ed9e0f0ca7c2d9f631b5a26d73fe3d7e0876d7a
IslamWalid <islamwalid83@gmail.com>
2023-07-22 18:22:21 +0300

    refactor: change delete query in categories seed down

--- src/seeders/20230302144747-categories.js
+++ src/seeders/20230302144747-categories.js
@@ -17,6 +17,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('categories');
+    await queryInterface.bulkDelete('categories', {}, { cascade: true });
   }
 };
