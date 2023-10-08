a7c4ad5a8aa5dde417fab24cb4dafa46b21e567f
IslamWalid <islamwalid83@gmail.com>
2023-10-08 17:08:49 +0300

    fix: hide postgres credentials

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,3 +1,22 @@
+require('dotenv').config();
+
 const config = require('config');
 
-module.exports = { ...config.get('dbConfig') };
+const username = config.has('dbConfig.username') ? config.get('dbConfig.username') : process.env.POSTGRES_USER;
+const password = config.has('dbConfig.password') ? config.get('dbConfig.password') : process.env.POSTGRES_PASSWORD;
+
+if (!username) {
+  console.error('postgres username is not set');
+  process.exit(1);
+}
+
+if (!password) {
+  console.error('postgres password is not set');
+  process.exit(1);
+}
+
+module.exports = {
+  ...config.get('dbConfig'),
+  username,
+  password
+};
