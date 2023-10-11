9cee59b0bab73e071d865ba35f7a53005eaf8008
IslamWalid <islamwalid83@gmail.com>
2023-10-11 23:24:35 +0300

    fix: handle `generateUser` fixture with no user passed

--- test/fixtures.js
+++ test/fixtures.js
@@ -3,14 +3,14 @@ const { faker } = require('@faker-js/faker');
 const { Roles } = require('../src/utils/enums');
 
 function generateUser(user) {
-  const role = user.role || (faker.datatype.boolean() ? Roles.CUSTOMER : Roles.SELLER);
+  const role = user?.role || (faker.datatype.boolean() ? Roles.CUSTOMER : Roles.SELLER);
 
   return {
-    username: user.username || faker.internet.userName(),
+    username: user?.username || faker.internet.userName(),
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
-    password: user.password || faker.internet.password(),
-    phone: user.phone || faker.phone.number('+201#########'),
+    password: user?.password || faker.internet.password(),
+    phone: user?.phone || faker.phone.number('+201#########'),
     address: faker.location.city(),
     role,
     shopName: role === Roles.SELLER ? faker.company.name() : null
