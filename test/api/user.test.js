17effca65ee4cebf49669275d3fde008053085fc
IslamWalid <islamwalid83@gmail.com>
2023-10-11 23:19:20 +0300

    test: organize and refactor api tests

--- test/api/user.test.js
+++ test/api/user.test.js
@@ -1,123 +1,71 @@
 require('dotenv').config();
 require('../../src/utils/check-env')();
 
-const crypto = require('crypto');
-
-const bcrypt = require('bcrypt');
 const req = require('supertest');
 
 const app = require('../../app');
-const { sequelize, User, Customer } = require('../../src/models/');
-const { Roles } = require('../../src/utils/enums');
+const { sequelize, User } = require('../../src/models/');
+const { Messages, HttpStatus } = require('../../src/utils/enums');
+const { generateUser } = require('../fixtures');
 
-beforeAll(async () => {
-  const id = crypto.randomUUID();
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash('StrongPassword123!', salt);
+describe('user endpoints', () => {
+  const newUser = generateUser();
 
-  await User.create({
-    id,
-    username: 'john_doe',
-    firstName: 'John',
-    lastName: 'Doe',
-    password: hash,
-    address: 'some address',
-    phone: '+201012345678',
-    role: Roles.CUSTOMER,
-    customer: { id }
-  },
-  {
-    include: Customer
-  });
-});
+  describe('user register', () => {
+    const uri = '/users/register';
 
-describe('user register', () => {
-  it('should register new user with valid input', async () => {
-    const reqBody = {
-      username: 'jane_doe',
-      firstName: 'Jane',
-      lastName: 'Doe',
-      password: 'AnotherStrongPassword123!',
-      phone: '+201087654321',
-      address: 'some home address',
-      role: Roles.SELLER,
-      shopName: 'offmarket'
-    };
-    const res = await req(app).post('/users/register').send(reqBody);
-    expect(res.statusCode).toBe(201);
+    let existingUser;
+
+    beforeAll(async () => {
+      existingUser = await User.findOne({ attributes: ['username', 'phone'], raw: true });
+    });
+
+    it('should register new user with return status 201', async () => {
+      const res = await req(app).post(uri).send(newUser);
+
+      expect(res.statusCode).toBe(HttpStatus.CREATED);
+    });
+
+    it('should try to register user with existing username and return 409 status code', async () => {
+      const res = await req(app).post(uri).send(generateUser({ username: existingUser.username }));
+
+      expect(res.statusCode).toBe(409);
+      expect(res.body.message).toBe(Messages.USERNAME_ALREADY_EXISTS);
+    });
+
+    it('should try to register user with existing phone and return 409 status code', async () => {
+      const res = await req(app).post('/users/register').send(generateUser({ phone: existingUser.phone }));
+
+      expect(res.statusCode).toBe(409);
+      expect(res.body.message).toBe(Messages.PHONE_ALREADY_EXISTS);
+    });
   });
 
-  it('should register new user with invalid user type', async () => {
-    const reqBody = {
-      username: 'john_lnu',
-      firstName: 'John',
-      lastName: 'Lnu',
-      password: 'AnotherStrongPassword123!',
-      phone: '+201012345678',
-      address: 'some home address',
-      role: 'invalid type',
-      shopName: 'offmarket'
-    };
-    const res = await req(app).post('/users/register').send(reqBody);
-    expect(res.statusCode).toBe(400);
-    expect(res.body.message).toBe('invalid input datatype');
+  describe('user login', () => {
+    it('should login with valid credentials', async () => {
+      const res = await req(app).post('/users/login').send({
+        username: newUser.username,
+        password: newUser.password
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.headers['set-cookie']).toBeDefined();
+    });
+
+    it('should login with invalid credentials', async () => {
+      const anotherUser = generateUser();
+      const res = await req(app).post('/users/login').send({
+        username: anotherUser.username,
+        password: anotherUser.password
+      });
+
+      expect(res.statusCode).toBe(401);
+      expect(res.body.message).toBe(Messages.INVALID_USERNAME);
+    });
   });
 
-  it('should register already existing user', async () => {
-    const reqBody = {
-      username: 'john_doe',
-      firstName: 'John',
-      lastName: 'Doe',
-      password: 'StrongPassword123!',
-      phone: '+201012345678',
-      address: 'some home address',
-      role: Roles.CUSTOMER
-    };
-    const res = await req(app).post('/users/register').send(reqBody);
-    expect(res.statusCode).toBe(409);
-    expect(res.body.message).toBe('username already exists');
+  afterAll(async () => {
+    await User.destroy({ where: { username: newUser.username } });
+    await sequelize.close();
   });
-
-  it('should register new user with already used phone number', async () => {
-    const reqBody = {
-      username: 'john_lnu',
-      firstName: 'John',
-      lastName: 'Lnu',
-      password: 'AnotherStrongPassword123!',
-      phone: '+201012345678',
-      address: 'some home address',
-      role: Roles.SELLER,
-      shopName: 'offmarket'
-    };
-    const res = await req(app).post('/users/register').send(reqBody);
-    expect(res.statusCode).toBe(409);
-    expect(res.body.message).toBe('phone already exists');
-  });
-});
-
-describe('user login', () => {
-  it('should login with valid credentials', async () => {
-    const reqBody = {
-      username: 'john_doe',
-      password: 'StrongPassword123!'
-    };
-    const res = await req(app).post('/users/login').send(reqBody);
-    expect(res.statusCode).toBe(200);
-    expect(res.headers['set-cookie']).toBeDefined();
-  });
-
-  it('should login with invalid credentials', async () => {
-    const reqBody = {
-      username: 'john_doe',
-      password: 'incorrect password'
-    };
-    const res = await req(app).post('/users/login').send(reqBody);
-    expect(res.statusCode).toBe(401);
-    expect(res.body.message).toBe('incorrect password');
-  });
-});
-
-afterAll(async () => {
-  await sequelize.query('TRUNCATE TABLE "user" CASCADE');
-  await sequelize.close();
 });
