04fed1944c760d4566ee3ce2c8bbf9d006d9abf7
IslamWalid <islamwalid83@gmail.com>
2023-10-11 18:50:14 +0300

    refactor: move user custom validators to user module

--- src/validations/user.js
+++ src/validations/user.js
@@ -1,15 +1,30 @@
 const joi = require('joi');
+const validator = require('validator');
 
-const { Roles } = require('../utils/enums');
+const { Roles, Messages } = require('../utils/enums');
 
-const { phone, password } = require('./custom');
+const validatePassword = (value, helper) => {
+  if (!validator.isStrongPassword(value)) {
+    return helper.message(Messages.WEAK_PASSWORD);
+  }
+
+  return value;
+};
+
+const validatePhone = (value, helper) => {
+  if (!validator.isMobilePhone(value, ['ar-EG'], { strictMode: true })) {
+    return helper.message(Messages.INVALID_PHONE);
+  }
+
+  return value;
+};
 
 const register = joi.object({
   username: joi.string().required(),
   firstName: joi.string().required(),
   lastName: joi.string().required(),
-  password: joi.string().required().custom(password),
-  phone: joi.string().required().custom(phone),
+  password: joi.string().required().custom(validatePassword),
+  phone: joi.string().required().custom(validatePhone),
   address: joi.string().required(),
   role: joi.string().required().valid(Roles.CUSTOMER, Roles.SELLER),
   shopName: joi.when('role', {
