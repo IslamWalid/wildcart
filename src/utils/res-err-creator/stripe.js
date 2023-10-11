eb129547515612c8de0eba782dbb047640177c55
IslamWalid <islamwalid83@gmail.com>
2023-10-11 15:41:53 +0300

    refactor: remove forgotten debug log statement

--- src/utils/res-err-creator/stripe.js
+++ src/utils/res-err-creator/stripe.js
@@ -1,7 +1,6 @@
 const { HttpStatus } = require('../enums');
 
 function stripeResErr(err) {
-  console.log('hehe');
   switch (err.type) {
     case 'StripeSignatureVerificationError':
       return { status: HttpStatus.FORBIDDEN, message: err.message, errInfo: err };
