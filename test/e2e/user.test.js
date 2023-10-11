1eb43603ae6e90c1311a9640e82ebd26a2d4d137
IslamWalid <islamwalid83@gmail.com>
2023-10-11 23:56:23 +0300

    test: make tests independent from database seeds

--- test/e2e/user.test.js
+++ test/e2e/user.test.js
@@ -7,18 +7,18 @@ const app = require('../../app');
 const { sequelize, User } = require('../../src/models/');
 const { Messages, HttpStatus } = require('../../src/utils/enums');
 const { generateUser } = require('../fixtures');
+const { createUser } = require('../../src/services/user');
 
 describe('user endpoints', () => {
   const newUser = generateUser();
+  const existingUser = generateUser();
+
+  beforeAll(async () => {
+    await createUser(existingUser);
+  });
 
   describe('user register', () => {
     const uri = '/users/register';
-
-    let existingUser;
-
-    beforeAll(async () => {
-      existingUser = await User.findOne({ attributes: ['username', 'phone'], raw: true });
-    });
 
     it('should register new user with return status 201', async () => {
       const res = await req(app).post(uri).send(newUser);
@@ -29,43 +29,49 @@ describe('user endpoints', () => {
     it('should try to register user with existing username and return 409 status code', async () => {
       const res = await req(app).post(uri).send(generateUser({ username: existingUser.username }));
 
-      expect(res.statusCode).toBe(409);
+      expect(res.statusCode).toBe(HttpStatus.CONFLICT);
       expect(res.body.message).toBe(Messages.USERNAME_ALREADY_EXISTS);
     });
 
     it('should try to register user with existing phone and return 409 status code', async () => {
-      const res = await req(app).post('/users/register').send(generateUser({ phone: existingUser.phone }));
+      const res = await req(app).post(uri).send(generateUser({ phone: existingUser.phone }));
 
-      expect(res.statusCode).toBe(409);
+      expect(res.statusCode).toBe(HttpStatus.CONFLICT);
       expect(res.body.message).toBe(Messages.PHONE_ALREADY_EXISTS);
     });
   });
 
   describe('user login', () => {
-    it('should login with valid credentials', async () => {
-      const res = await req(app).post('/users/login').send({
-        username: newUser.username,
-        password: newUser.password
+    const uri = '/users/login';
+
+    it('should login with valid credentials and return status code 200', async () => {
+      const res = await req(app).post(uri).send({
+        username: existingUser.username,
+        password: existingUser.password
       });
 
-      expect(res.statusCode).toBe(200);
+      expect(res.statusCode).toBe(HttpStatus.OK);
       expect(res.headers['set-cookie']).toBeDefined();
     });
 
-    it('should login with invalid credentials', async () => {
+    it('should login with invalid credentials and return status code 401', async () => {
       const anotherUser = generateUser();
-      const res = await req(app).post('/users/login').send({
+      const res = await req(app).post(uri).send({
         username: anotherUser.username,
         password: anotherUser.password
       });
 
-      expect(res.statusCode).toBe(401);
+      expect(res.statusCode).toBe(HttpStatus.UNAUTHORIZED);
       expect(res.body.message).toBe(Messages.INVALID_USERNAME);
     });
   });
 
   afterAll(async () => {
-    await User.destroy({ where: { username: newUser.username } });
+    await User.destroy({
+      where: {
+        username: [newUser.username, existingUser.username]
+      }
+    });
     await sequelize.close();
   });
 });
