c31a5de85469766e928eda770b22ed256f7269f4
IslamWalid <islamwalid83@gmail.com>
2023-10-13 00:22:13 +0300

    chore: prepare product endpoints e2e fixture tests

--- test/fixtures.js
+++ test/fixtures.js
@@ -17,6 +17,31 @@ function generateUser(user) {
   };
 }
 
+function generateProduct(product) {
+  return {
+    name: product?.name || faker.commerce.productName(),
+    sellerId: product?.sellerId || faker.string.uuid(),
+    price: faker.number.int({ min: 10, max: 200 }),
+    quantity: faker.number.int({ min: 1, max: 50 }),
+    brand: faker.company.name(),
+    avgRate: faker.number.float({ min: 1, max: 5, precision: 2 }),
+    categories: product?.categories || generateMultiple(generateCategory, faker.number.int({ min: 1, max: 3 }))
+  };
+}
+
+function generateCategory(category) {
+  return {
+    name: category?.name || `${faker.commerce.department()}-${Date.now()}`
+  };
+}
+
+function generateMultiple(generator, count, obj) {
+  return faker.helpers.multiple(() => generator(obj), { count });
+}
+
 module.exports = {
-  generateUser
+  generateUser,
+  generateProduct,
+  generateCategory,
+  generateMultiple
 };
