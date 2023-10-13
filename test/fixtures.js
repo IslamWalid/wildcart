19db80b8d9dd2f57c97b1342c5374308651c108c
IslamWalid <islamwalid83@gmail.com>
2023-10-14 02:00:10 +0300

    fix: make product categories array of strings

--- test/fixtures.js
+++ test/fixtures.js
@@ -10,7 +10,7 @@ function generateUser(user) {
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
     password: user?.password || 'StrongPassword@12345!!',
-    phone: user?.phone || faker.phone.number('+2010########'),
+    phone: user?.phone || faker.helpers.replaceSymbolWithNumber('+2010########'),
     address: faker.location.city(),
     role,
     shopName: role === Roles.SELLER ? faker.company.name() : null
@@ -19,19 +19,22 @@ function generateUser(user) {
 
 function generateProduct(product) {
   return {
+    id: faker.string.uuid(),
     name: product?.name || faker.commerce.productName(),
     sellerId: product?.sellerId || faker.string.uuid(),
     price: faker.number.int({ min: 10, max: 200 }),
     quantity: faker.number.int({ min: 1, max: 50 }),
     brand: faker.company.name(),
     avgRate: faker.number.float({ min: 1, max: 5, precision: 2 }),
-    categories: product?.categories || generateMultiple(generateCategory, faker.number.int({ min: 1, max: 3 }))
+    categories: product?.categories ||
+      generateMultiple(generateCategory, faker.number.int({ min: 1, max: 3 }))
+        .map((_) => _.name)
   };
 }
 
 function generateCategory(category) {
   return {
-    name: category?.name || `${faker.commerce.department()}-${Date.now()}`
+    name: category?.name || `${faker.commerce.department()}-${faker.number.int({ min: 1, max: 1000 })}`
   };
 }
 
